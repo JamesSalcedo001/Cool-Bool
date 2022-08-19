f1fdd7801b226fc073f98977c6af3a4b57eccaa5
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-08-18 19:52:17 -0500

    attempt to fix broken img

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "CoolCats ONLY"
 
 document.querySelector('body').appendChild(h2);
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
